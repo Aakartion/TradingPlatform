0772aced8b8713a63951490c08c59ffda65528be
aakarshan <aakartionsimkhada@gmail.com>
2024-11-21 14:18:59 -0600

    Implementaion class for CoinService is created

--- src/main/java/edu/miu/TradingPlatform/service/coins/impl/CoinsServiceImpl.java
+++ src/main/java/edu/miu/TradingPlatform/service/coins/impl/CoinsServiceImpl.java
@@ -1,4 +1,53 @@
 package edu.miu.TradingPlatform.service.coins.impl;
 
-public class CoinsServiceImpl {
+import edu.miu.TradingPlatform.domain.Coins;
+import edu.miu.TradingPlatform.repository.CoinsRepository;
+import edu.miu.TradingPlatform.service.coins.CoinsService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CoinsServiceImpl implements CoinsService {
+
+    private final CoinsRepository coinsRepository;
+
+    public CoinsServiceImpl(CoinsRepository coinsRepository) {
+        this.coinsRepository = coinsRepository;
+    }
+
+    @Override
+    public List<Coins> getCoinList(int page) {
+        return List.of();
+    }
+
+    @Override
+    public String getMarketChart(String coinId, int days) {
+        return "";
+    }
+
+    @Override
+    public String getCoinsDetails(String coinId) {
+        return "";
+    }
+
+    @Override
+    public Coins findCoinByCoinId(String coinId) {
+        return null;
+    }
+
+    @Override
+    public String searchCoin(String keyword) {
+        return "";
+    }
+
+    @Override
+    public String getTop50CoinsByMarketCapRank() {
+        return "";
+    }
+
+    @Override
+    public String getTradingCoins() {
+        return "";
+    }
 }
